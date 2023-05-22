057040249f229894fb233773c69e96859759d680
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-05-22 23:19:50 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,30 @@
-export const App = () => {
+import { Component } from "react";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+export class App extends Component {
+
+state = {
+  contacts: [],
+  name: ''
+}
+  
+  
+  render() {
+    
+    
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const form = e.currentTarget;
+      const nameValue = form.elements[0].value;
+      console.log(form.elements[0].value);
+      
+      this.setState({})
+      this.setState(this.state.contacts.push({}))
+    };
+
+  
+console.log(this.state)
   return (
     <div
       style={{
@@ -7,10 +33,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection:'column'
       }}
     >
-      React homework template
+      <h2>Phonebook</h2>
+      <ContactForm submit={handleSubmit}  />
+      <h2>Contacts</h2>
+      <ContactList/>
     </div>
   );
-};
+  }
+
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import styles from './ContactList.module.css';
+
+export class ContactList extends Component{
+
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    
+                </ul>
+        </div>
+    )
+}
+
+}
