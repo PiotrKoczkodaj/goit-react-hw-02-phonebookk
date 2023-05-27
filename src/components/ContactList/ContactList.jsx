b0d7a987f0404eb5f1bb9fabdf9d4842efa75b34
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-05-27 14:28:19 +0200

    .

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,15 +4,21 @@ import { nanoid } from 'nanoid';
 export class ContactList extends Component {
   render() {
     const { state } = this.props;
-   
     return (
-      
       <div>
         {state.contacts.map(contact => (
           <p key={nanoid()}>
             {contact.name}
             {contact.number}
-            <button  value={contact.name}>delete</button>
+            <button
+              onClick={() => {
+                let index = state.contacts.indexOf(contact);
+                this.setState(state.contacts.splice(index, 1));
+              }}
+              value={contact.name}
+            >
+              delete
+            </button>
           </p>
         ))}
       </div>
