899df02b22bd6b6df0e89d231ea7d9ec35474d65
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-05-28 15:35:08 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,31 +16,13 @@ export class App extends Component {
   };
 
   render() {
-    
-    // const handleSubmit = e => {
-    //   e.preventDefault();
-    //   const form = e.currentTarget;
-    //   const nameValue = form.elements[0].value;
-    //   const number = form.elements[1].value;
-
-
-    // };
-
     const handleSubmit = e => {
       e.preventDefault();
       const form = e.currentTarget;
       const nameValue = form.elements[0].value;
       const number = form.elements[1].value;
 
-      
-      this.setState(prevState => {
-        prevState.contacts.map(contact => {
-          if (contact.name.includes(nameValue)) {
-           return alert('juz jest')
-          }
-
-          if (!contact.name.includes(nameValue)) {
-                 this.setState({
+      this.setState({
         contacts: [
           ...this.state.contacts,
           {
@@ -51,48 +33,48 @@ export class App extends Component {
         ],
         filter: nameValue,
       });
-          }
-       
-    })
-  });
-}
-    
-    
 
-    const changingState = (e) => {
-       this.setState({
-            filter:e.target.value.toUpperCase()
-       });
-     
+      this.state.contacts.map(contact => {
+        if (contact.name.includes(nameValue)) {
+          this.setState({
+            contacts: this.state.contacts,
+          });
+          return alert(`${nameValue} is already in contacts`);
+        }
+      });
+    };
+
+    const changingState = e => {
+      this.setState({
+        filter: e.target.value.toUpperCase(),
+      });
+
       let result = this.state.contacts.filter(contact => {
-       
-        return contact.name.toUpperCase().includes(this.state.filter)
-        
+        return contact.name.toUpperCase().includes(this.state.filter);
+      });
+
+      this.setState({
+        contacts: result,
       });
       
-      this.setState({
-            contacts:result
-      });
-        
-    }    
-   
-  return (
+    };
+    return (
       <div
         style={{
           height: '100vh',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-        fontSize: 40,
+          fontSize: 40,
           color: '#010101',
           flexDirection: 'column',
         }}
       >
         <h2>Phonebook</h2>
-        <ContactForm submit={handleSubmit}  />
+        <ContactForm submit={handleSubmit} />
         <h2>Contacts</h2>
-        <Filter state={this.state} func={changingState}  />
-        <ContactList state={this.state}/>
+        <Filter state={this.state} func={changingState} />
+        <ContactList state={this.state} />
       </div>
     );
   }
